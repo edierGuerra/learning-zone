1057fc8f71c6c74f967bc176174dc40edc64c25f
Charif Tatiana <sharifgiraldo496@gmail.com>
2025-05-29 15:55:43 -0500

    feat(components): Se creo el componente de Home Global y el Home del Usuario

---
+++ frontend/src/componentes/pages/usuariohome/UsuarioHome.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const UsuarioHome: React.FC = () => {
+  return (
+    <div>
+      <h1>Learning zone</h1>
+      <div>
+        <h2>Bienvenido</h2>
+        <p>Selecciona un curso para comenzar</p>
+      </div>
+      </div>
+  )
+  
+}
+
+
+export default UsuarioHome;

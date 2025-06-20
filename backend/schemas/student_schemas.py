749346f5defe95992e91edfc85ed7969f483bc4f
Edier <edierguerra55@gmail.com>
2025-06-20 12:05:37 -0500

    feat(schemas): Agrega los esquemas del estudiante para su validación

---
+++ backend/schemas/student_schemas.py
@@ -0,0 +1,30 @@
+#user_schemas.py
+""" 
+Este módulo define esquemas de validación y respuesta para la gestión de datos de estudiantes utilizando la biblioteca pydantic.
+"""
+
+from pydantic import BaseModel, Field, EmailStr
+
+class StudentRegister(BaseModel):
+    """
+    Esquema para validar los datos de registro de un nuevo estudiante.
+    """
+    identification_number: int = Field(..., title="Número de Identificación", description="Número único del estudiante")
+    names: str = Field(...,min_length=2, max_length=50, title="Nombres", description="Nombres del estudiante")
+    last_names: str = Field(...,min_length=2, max_length=50, title="Apellidos", description="Apellidos del estudiante")
+    email: EmailStr = Field(..., title="Correo Electrónico", description="Correo electrónico válido del estudiante")
+    password: str = Field(...,min_length=6, title="Contraseña", description="Contraseña con mínimo 6 caracteres")
+    identification_id:int = Field(..., title="Id del número de Identificación", description="Clave foranea del numero de identificación.")
+
+class StudentResponse(BaseModel):
+    """
+    Esquema de salida para representar un estudiante registrado (sin contraseña).
+    """
+    id: int = Field(..., title="ID", description="Identificador único del estudiante")
+    identification_number: int = Field(..., title="Número de Identificación")
+    names: str = Field(..., title="Nombres")
+    last_names: str = Field(..., title="Apellidos")
+    email: EmailStr = Field(..., title="Correo Electrónico")
+
+    class Config:
+        from_atributes = True  # Permite que Pydantic convierta objetos ORM (como los de SQLAlchemy)

c177e1b372cd6ae648573dc2993d34f576f38a48
camilo <ospinavillacamiloandres@gmail.com>
2025-06-21 21:19:57 -0500

    chore(backend): agregando comentarios a los archivos config_db, config

--- backend/config.py
+++ backend/config.py
@@ -1,10 +1,13 @@
-# app/config.py
+# config.py
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing import Optional
 
 class Settings(BaseSettings): #clase de configuracion heredando BaseSettings
     # Esto le dice a Pydantic que busque el archivo .env
+    """
+    este modulo contiene la conexion al archivo .env para traer la url de la conexion a la base de datos
+    """
     model_config = SettingsConfigDict( #aca se configura como base settings cargara los valores
         env_file='.env', #le indica a pydantic que busque un archivo .env en el directorio actual
         env_file_encoding='utf-8', # codificacion del archivo .env

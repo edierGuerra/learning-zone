8a0e11ef4171a67a6ddc34adf96e7c7b6cece98e
camilo <ospinavillacamiloandres@gmail.com>
2025-06-21 21:28:21 -0500

    feat(repository): agrega el repositorio de identificacion para obtener el objeto de la base de datos

---
+++ backend/repository/identification_repository.py
@@ -0,0 +1,24 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from models.identification_model import Identification
+
+class IdentificationRepository:
+    """
+    Repositorio encargado de acceder a los datos del modelo 'Identification' en la base de datos.
+
+    Este patrón organiza las operaciones relacionadas con la persistencia de datos,
+    separándolas de la lógica de negocio y de las rutas (API).
+    Centraliza todas las conexiones a la base de datos separandolas del resto
+    
+    - Esta clase retornara Un objeto del modelo 'Identification' si se encuentra, sino devuelve None.
+    """
+    @staticmethod #especifica una funcion normal dentro de una clase
+    async def get_by_code(session: AsyncSession, code: int) -> Identification | None: #esta funcion devuelve un objeto de tipo Identification o None
+        # Buscar el número de identificación en la base de datos
+        result = await session.execute( #Gracias a session no hay necesidad de abrir o cerrar manualmente la conexión.
+            select(Identification).where(Identification.n_identification == code)
+            #select moderno de sqlalchemy +1.4 compatible con programacion asincrona para no escribir consultas sql crudas
+        )
+        return result.scalars().first()
+        # scalars extrae solo el objeto modelo (evita tuplas complejas o innecesarias).
+        # first trae el primer resultado o none

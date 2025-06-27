f4408727994cc1a1781c8bd19345f02dc3274b62
Edier <edierguerra55@gmail.com>
2025-06-27 14:53:51 -0500

    feat(schemas): Agrega eaquema para validar los datos del login del usuario

--- backend/schemas/student_schemas.py
+++ backend/schemas/student_schemas.py
@@ -1,30 +1,33 @@
 #student_schemas.py
-""" 
+''' 
 Este módulo define esquemas de validación y respuesta para la gestión de datos de estudiantes utilizando la biblioteca pydantic.
-"""
+'''
 
 from pydantic import BaseModel, Field, EmailStr
 
 class StudentRegister(BaseModel):
-    """
+    '''
     Esquema para validar los datos de registro de un nuevo estudiante.
-    """
-    identification_number: int = Field(..., title="Número de Identificación", description="Número único del estudiante")
-    names: str = Field(...,min_length=2, max_length=50, title="Nombres", description="Nombres del estudiante")
-    last_names: str = Field(...,min_length=2, max_length=50, title="Apellidos", description="Apellidos del estudiante")
-    email: EmailStr = Field(..., title="Correo Electrónico", description="Correo electrónico válido del estudiante")
-    password: str = Field(...,min_length=6, title="Contraseña", description="Contraseña con mínimo 6 caracteres")
-    identification_id:int = Field(..., title="Id del número de Identificación", description="Clave foranea del numero de identificación.")
+    '''
+    identification_number: int = Field(..., title='Número de Identificación', description='Número único del estudiante')
+    names: str = Field(...,min_length=2, max_length=50, title='Nombres', description='Nombres del estudiante')
+    last_names: str = Field(...,min_length=2, max_length=50, title='Apellidos', description='Apellidos del estudiante')
+    email: EmailStr = Field(..., title='Correo Electrónico', description='Correo electrónico válido del estudiante')
+    password: str = Field(...,min_length=6, title='Contraseña', description='Contraseña con mínimo 6 caracteres')
+    identification_id:int = Field(..., title='Id del número de Identificación', description='Clave foranea del numero de identificación.')
 
+class StudentLogin(BaseModel):
+    email:str = Field(..., title='Correo Electronico', description='Correo electronico del usuario ya registrado.')
+    password:str = Field(..., title='Contraseña', description='Contraseña del estudiante ya registrado')
 class StudentResponse(BaseModel):
-    """
+    '''
     Esquema de salida para representar un estudiante registrado (sin contraseña).
-    """
-    id: int = Field(..., title="ID", description="Identificador único del estudiante")
-    identification_number: int = Field(..., title="Número de Identificación")
-    names: str = Field(..., title="Nombres")
-    last_names: str = Field(..., title="Apellidos")
-    email: EmailStr = Field(..., title="Correo Electrónico")
+    '''
+    id: int = Field(..., title='ID', description='Identificador único del estudiante')
+    identification_number: int = Field(..., title='Número de Identificación')
+    names: str = Field(..., title='Nombres')
+    last_names: str = Field(..., title='Apellidos')
+    email: EmailStr = Field(..., title='Correo Electrónico')
 
     class Config:
         from_attributes = True  # Permite que Pydantic convierta objetos ORM (como los de SQLAlchemy)

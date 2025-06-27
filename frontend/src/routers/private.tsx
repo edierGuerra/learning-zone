d2497e57516af79f6e1f0d3002520ef1cf3f0dba
Junior Herrera <herrerajunior715@gmail.com>
2025-06-27 17:40:22 -0500

    refactor(frontend): Reorganización de rutas privadas y públicas
    - Se unifica y centraliza la navegación basada en la autenteticación
    - Se mejora el control de acceso a través de PrivateRouters
    - Estructura más clara entre rutas públicas y privadas

--- frontend/src/routers/private.tsx
+++ frontend/src/routers/private.tsx
@@ -3,15 +3,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthLayout from "../shared/Layouts/AuthLayout";
 import Home from "../modules/dashboard/pages/Home";
+import UserPage from "../modules/user/userPage";
+import WordHomePage from "../modules/courses/word/pages/WordHomePage";
+import ExcelHomePage from "../modules/courses/excel/pages/ExcelHomePage";
+import PowerPointHomePage from "../modules/courses/powerpoint/pages/PowerPointHomePage";
 
 
 export default function RoutersPrivates() {
   return (
     <BrowserRouter>
         <Routes>
-            <Route element={<AuthLayout/>}>
-                <Route path="/home" element={<Home/>}/>
-            </Route>
+              <Route element={<AuthLayout/>}>
+                  <Route path="/home" element={<Home/>}/>
+                  <Route path="/userPage" element={<UserPage/>}/>
+                  <Route path="/word" element={<WordHomePage/>}/>
+                  <Route path="/excel" element={<ExcelHomePage/>}/>
+                  <Route path="/powerPoint" element={<PowerPointHomePage/>}/>
+              </Route>
         </Routes>
     
     </BrowserRouter>

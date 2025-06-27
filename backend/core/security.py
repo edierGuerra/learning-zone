33321995946cbdddfd3e9e98a9bd138e8e1eec58
Edier <edierguerra55@gmail.com>
2025-06-27 12:37:02 -0500

    feat(security): Agrega función para obtener los datos del estudiante por medio del token de acceso

--- backend/core/security.py
+++ backend/core/security.py
@@ -1,16 +1,25 @@
 #core/security.py
 
-
 # Modulos externos
 import os
 from dotenv import load_dotenv
 from datetime import datetime, timedelta, timezone
-
 from jose import jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security.oauth2 import OAuth2PasswordBearer
+
+# Modulos internos
+from dependencies.student_dependencie import get_student_services
+from services.student_services import StudentService
+
+# Generación y manipulación del token
+
+oauth2_scheme_register = OAuth2PasswordBearer(tokenUrl='api/v1/student/verify_email')
 
 load_dotenv()
 MY_TOKEN_KEY = os.getenv("TOKEN_KEY")
 ALGORITHM = "HS256"
+
 def encode_access_token(payload:dict, exp_time:int = 3600) -> str:
     """  
     Codifica un token de acceso.
@@ -20,7 +29,7 @@ def encode_access_token(payload:dict, exp_time:int = 3600) -> str:
         exp_time(int): Tiempo de expiración del token expresado en segundos, por defecto 3600
 
     Returns:
-        token(str): El token ya encriptado con la información del usuario
+        token(str): El token ya encriptado con la información del estudiante
 
     """
     # Copia de pyload
@@ -37,3 +46,42 @@ def encode_access_token(payload:dict, exp_time:int = 3600) -> str:
     # Configruacion del token
     token = jwt.encode(to_encode,MY_TOKEN_KEY,ALGORITHM)
     return token
+
+async def get_current_student(token:str = Depends(oauth2_scheme_register), services:StudentService = Depends(get_student_services)):
+    """ 
+    ## Obtener Estudiante 
+    
+    Permite obtener un estudiante atravez de un access token.
+    
+    ### Parametros:
+    `token(str)`: Token que contiene el id del estudiante codificado.
+    `services(StudentServices)`: Servicio que contiene las diferentes funcionalidades del estudiante.
+    
+    ### Retorna:
+    `student(Student`: Objeto de tipo estudiante.
+    
+    ### Excepciones:
+        `status(404)`: En caso de no econtrar el estudiante.
+        `status(401)`: En caso de que el token sea invalido o el estudiante no este autorizado.
+    """
+    # Desencripta el token y obtiene el id 
+    pyload = jwt.decode(token=token, key=MY_TOKEN_KEY, algorithms=[ALGORITHM])
+    user_id = pyload.get('sub')
+    
+    # Lanza excepción en caso de no obtener el id del estudiante
+    if user_id is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Token inválido o estudiante no autorizado')
+    
+    # Ejecutamos en caso de obtener el id del estudiante
+    elif user_id is not None:
+        id_to_valid = int(user_id) # Convertir el id del estudiante a entero
+        
+        # Obtener el estudiante de la base de datos
+        student = await services.get_student_by_id(id=id_to_valid)
+        
+        # Arrojar excepción en caso de no encontrar el estudiante
+        if not student:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Estudiante no encontrado')
+        
+        # En caso de que todo este bien retornar el estudiante
+        return student

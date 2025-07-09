172ebfc4f178af61ee6695c18062200a28b5ecbf
Junior Herrera <herrerajunior715@gmail.com>
2025-07-09 13:32:47 -0500

    refactor(notifications): integrar vista de notificaciones al layout autenticado

--- frontend/src/shared/Layouts/AuthLayout.tsx
+++ frontend/src/shared/Layouts/AuthLayout.tsx
@@ -3,8 +3,10 @@ import Header from "../Components/Header";
 /* import SplashCursor from "../animations/SplashCursor"; */
 import './styles/PrivateLayout.css'
 import Footer from "../Components/Footer";
+import useNotifications from "../../modules/notifications/hooks/useNotifications";
 
 export default function AuthLayout() {
+  useNotifications(); /* Uso del hook de notifications para que siempre se ejecute mientras este logueado */
   return (
     <div className="private-layout">
       <Header isPublic ={false}/>

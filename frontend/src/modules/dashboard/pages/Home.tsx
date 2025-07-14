214b653207169a3f3f29069d2268c2fa4e035407
Junior Herrera <herrerajunior715@gmail.com>
2025-07-14 09:29:13 -0500

    refactor(dashboard): actualiza componente de cursos y página de inicio

--- frontend/src/modules/dashboard/pages/Home.tsx
+++ frontend/src/modules/dashboard/pages/Home.tsx
@@ -1,26 +1,46 @@
-import { useUser } from '../../auth/Hooks/useAuth'
-import CardCourse from '../Components/CardCourse'
-import word from '../../../assets/learningZone/img-word-course.png'
-import powerpoint from '../../../assets/learningZone/img-powerpoint-course.png'
-import excel from '../../../assets/learningZone/img-excel-course.png'
+import { useUser } from '../../auth/Hooks/useAuth';
+import CardCourse from '../Components/CardCourse';
+import word from '../../../assets/learningZone/img-word-course.png';
+import powerpoint from '../../../assets/learningZone/img-powerpoint-course.png';
+import excel from '../../../assets/learningZone/img-excel-course.png';
 
-import '../Styles/Home.css'
+import '../Styles/Home.css';
+import { useCourseContext } from '../../courses/hooks/useCourse';
+
 export default function Home() {
-  const {student} = useUser()
+  const { student } = useUser();
+  const { courses } = useCourseContext();
+
+  // Relación entre nombre y su imagen correspondiente
+  const courseImages: Record<string, string> = {/* Record crea un objeto con claves del tipo K y valores del tipo T. */
+    excel,
+    word,
+    'powerpoint': powerpoint,
+  };
+
   return (
     <div className="container-home-user">
-      <h1 className='home-title'>Bienvenido {student?.name} </h1>
+      <h1 className="home-title">Bienvenido {student?.name}</h1>
       <p>Selecciona un curso para comenzar</p>
+
       <div className="container-courses">
-        <CardCourse name='excel' description='Curso basico de Excel para 
-        manipular datos' image={excel}/>
+        {courses.map(course => {
+          const nameKey = course.name.toLowerCase();
+          const image = courseImages[nameKey]; /* Traer la imagen correspondiente a el nombre que venga del backend */
 
-        <CardCourse name='word' description='Curso basico de Word para
-        crear documentos' image={word}/>
+          if (!image) return null; // Si no hay imagen asignada, no renderiza nada
 
-        <CardCourse name='powerpoint' description='Curso basico de Power Point 
-        para crear presentaciones.' image={powerpoint}/>
+          return (
+            <CardCourse
+              key={course.id}
+              id ={course.id}
+              name={course.name}
+              description={course.description}
+              image={image}
+            />
+          );
+        })}
       </div>
     </div>
-  )
+  );
 }

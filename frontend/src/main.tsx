3973b3d258445cf3a3af2bb2a64180450ebbfe88
Junior Herrera <herrerajunior715@gmail.com>
2025-07-14 09:40:12 -0500

    refactor(main): actualiza punto de entrada principal

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { StudentProvider } from './modules/auth/Context/userProvider';
 import App from './App';
+import { CourseProvider } from './modules/courses/context/CourseProvider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <StudentProvider> {/* 👈 Esto es obligatorio */}
-        <App />
+        <CourseProvider>
+          <App />
+        </CourseProvider>
       </StudentProvider>
     </BrowserRouter>
 );

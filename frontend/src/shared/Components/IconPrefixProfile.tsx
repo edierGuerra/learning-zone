733dec3882faf9476ad4a67d2bce559ae2c62284
Charif Tatiana <sharifgiraldo496@gmail.com>
2025-07-14 22:26:00 -0500

    Refactor(shared) refactorizando el envío de los parámetros de la función GenerateColorFromName

--- frontend/src/shared/Components/IconPrefixProfile.tsx
+++ frontend/src/shared/Components/IconPrefixProfile.tsx
@@ -2,10 +2,9 @@ import { useUser } from "../../modules/auth/Hooks/useAuth"
 import GenerateColorFromName from "../Utils/GenerateColorFromName";
 export default function IconPrefixProfile() {
     const {student} = useUser()
-    const color = GenerateColorFromName(student?.name ??"", student?.id ?? 0);
-
+    const color = GenerateColorFromName(student?.name ??"", student?.numIdentification ?? 0);
   return (
-    <div style={{'backgroundColor':`${color}`, 'color':'#fff',  'height':'62px', 'width':'62px','borderRadius':'50%',  'alignContent':'center', 'justifyItems':'center', 'textAlign':'center'}}>
+    <div style={{'backgroundColor':color, 'color':'#fff',  'height':'62px', 'width':'62px','borderRadius':'50%',  'alignContent':'center', 'justifyItems':'center', 'textAlign':'center'}}>
         <p style={{'color':'fff', 'fontSize':'26px'}}>{student?.prefixProfile}</p>
     </div>
   )

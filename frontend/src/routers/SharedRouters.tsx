a0609c6dcdd5d7be8e4f13783ef8bd0ac6e971c2
Junior Herrera <herrerajunior715@gmail.com>
2025-07-25 11:14:13 -0500

    (feat(routers) mejora en la implementacion de rutas

--- frontend/src/routers/SharedRouters.tsx
+++ frontend/src/routers/SharedRouters.tsx
@@ -4,9 +4,15 @@ import { Route } from "react-router-dom";
 import InstitutionPage from "../pages/about/Institution";
 import { AboutUs } from "../pages/about/TeamInfo";
 import { SitePolicies } from "../pages/info/SitePolicies";
+import ComplaintPage from "../pages/feedbackform/ComplaintPage";
+import errorsRouters from "./Errors";
 
 export const sharedRoutes = [
   <Route key="aboutInstitution" path="/aboutInstitution" element={<InstitutionPage />} />,
   <Route key="aboutUs" path="/aboutUs" element={<AboutUs />} />,
   <Route key="sitePolicies" path="/sitePolicies" element={<SitePolicies />} />,
+  <Route key="siteSugerences" path="/siteSugerences" element={<ComplaintPage />} />,
+  ...errorsRouters
+  
+
 ];

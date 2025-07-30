3396b1ca415d6dfa05270f497a464414e58b9e99
Junior Herrera <herrerajunior715@gmail.com>
2025-07-30 10:59:12 -0500

    chore(routing): limpiar rutas obsoletas y adaptar StudentRoutes a nuevo flujo de cursos

--- frontend/src/routers/StudentRoutes.tsx
+++ frontend/src/routers/StudentRoutes.tsx
@@ -1,18 +1,19 @@
 import { Route, Routes } from "react-router-dom";
-import CourseRouter from "./CourseRouter";
 import ContentPage from "../modules/courses/components/ContentPage";
 import EvaluationPage from "../modules/courses/components/EvaluationPage";
 import CommentPageWrapper from "../modules/courses/comments/CommentPageWrapper";
 import HomeStudent from "../modules/student/pages/HomeStudent";
-import StudentPage from "../modules/student/pages/StudentPage";
+import StudentProfile from "../modules/student/pages/StudentProfile";
+import CourseHomePage from "../modules/courses/pages/HomeCourse";
+
 
 export default function StudentRouters() {
   return (
     <Routes>
       {/* rutas del estudiante */}
         <Route path="/homeStudent" element={<HomeStudent />} />
-        <Route path="/profileStudent" element={<StudentPage/>}/> {/* Perfil del estudiante */}
-        <Route path="/courses/:nameCourse" element={<CourseRouter />} />
+        <Route path="/profileStudent" element={<StudentProfile/>}/> {/* Perfil del estudiante */}
+        <Route path="/course/:id/:nameCourse" element={<CourseHomePage />} />
         <Route path="/contentPage" element={<ContentPage/>}/>
         <Route path="/evaluationPage" element={<EvaluationPage/>}/>
         <Route path="/comments/:courseId" element={<CommentPageWrapper />} />
--- frontend/src/shared/Utils/authStorage.tsx
+++ frontend/src/shared/Utils/authStorage.tsx
@@ -1,9 +1,10 @@
 // PARA MAYOR SEGURIDAD MIGRAR ESTO A COOKIES
 // Importa los tipos de datos definidos para el usuario y su token
 import type { TComment, TStudentAllComents } from "../../modules/courses/comments/types";
-import type { TContent, TCourses, TEvaluation, TLesson, TLessons } from "../../modules/courses/types/Course";
+import type { TContent, TCoursesStudents, TCoursesTeachers, TEvaluation, TLesson, TLessons } from "../../modules/courses/types/Course";
 import type { TNotifications } from "../../modules/notifications/types/Notifications";
 import type { TStudent, TStudentConsent, TStudentProfile, TStudentProfileToken, TUser, TUserRole } from "../../modules/types/User";
+import type { TColorPalette } from "../theme/ColorPalettesCourses";
 
 // Objeto que encapsula el acceso al almacenamiento local (localStorage)
 export const authStorage = {
@@ -152,20 +153,35 @@ export const authStorage = {
 
   removeNotifications: () => localStorage.removeItem('notifications'),
 
-  setCourse:(courses:TCourses)=>{
-    localStorage.setItem('courses',JSON.stringify(courses))
+  setCourseStudent:(courses:TCoursesStudents)=>{
+    localStorage.setItem('coursesStudent',JSON.stringify(courses))
   },
-  getCourses:():TCourses =>{
+  getCoursesStudent:():TCoursesStudents =>{
     try {
-      const raw = localStorage.getItem("courses");
-      return raw ? (JSON.parse(raw) as TCourses) : [];
+      const raw = localStorage.getItem("coursesStudent");
+      return raw ? (JSON.parse(raw) as TCoursesStudents) : [];
     } catch (e) {
       console.error("El JSON está mal formado:", e);
       return [];
     }
 
   },
-  removeCourses:()=>localStorage.removeItem('courses'),
+  removeCoursesStudent:()=>localStorage.removeItem('coursesStudent'),
+
+  setCoursesTeacher:(courses:TCoursesTeachers)=>{
+    localStorage.setItem('coursesStudent',JSON.stringify(courses))
+  },
+  getCoursesTeacher:():TCoursesTeachers =>{
+    try {
+      const raw = localStorage.getItem("coursesTeacher");
+      return raw ? (JSON.parse(raw) as TCoursesTeachers) : [];
+    } catch (e) {
+      console.error("El JSON está mal formado:", e);
+      return [];
+    }
+
+  },
+  removeCoursesTeacher:()=>localStorage.removeItem('coursesTeacher'),
 
 
   setLessons:(lessons:TLessons)=>{
@@ -232,4 +248,21 @@ export const authStorage = {
 
   // Elimina la evaluation del localStorage
   removeEvaluation: () => localStorage.removeItem("evaluation"),
+
+  setPaletteColors :(palette:TColorPalette)=>
+    localStorage.setItem('paletteColors',JSON.stringify(palette)),
+  // Recupera el objeto de de la paleta de colores desde localStorage
+  getPaletteColors: (): TColorPalette | null => {
+    try {
+      const raw = localStorage.getItem("paletteColors");
+      return raw ? (JSON.parse(raw) as TColorPalette) : null;
+    } catch (e) {
+      console.error("El JSON está mal formado :", e);
+      return null;
+    }
+  },
+   
+   // Elimina la paleta de colores  del localStorage
+  removePaletteColors: () => localStorage.removeItem("paletteColors"),
+
 };

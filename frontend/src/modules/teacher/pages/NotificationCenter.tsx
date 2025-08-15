7285c0140686ad74dd28eb1f8207a6cc0c212519
Junior Herrera <herrerajunior715@gmail.com>
2025-08-15 09:13:02 -0500

    chore(cleanup): Se eliminaron archivos y recursos obsoletos

--- frontend/src/modules/teacher/pages/NotificationCenter.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import NotificationCreator from '../components/NotificationCreator';
-
-export default function NotificationCenter() {
-  return <NotificationCreator />;
-}
--- frontend/src/modules/teacher/pages/StudentProgress.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import StudentProgress from '../components/StudentsProgress';
-
-export default function StudentProgressPage() {
-  return <StudentProgress />;
-}
